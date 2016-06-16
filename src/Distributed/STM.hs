72c100382dfda4b971fb0f3daeeca7127040261a
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-06-16 16:28:47 +0300

    WIP

--- src/Distributed/STM.hs
+++ src/Distributed/STM.hs
@@ -1,3 +1,24 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Distributed.STM where
 
+import           Control.Monad.RWS
 
+import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.Transaction
+
+newtype Atom a = Atom (RWST Connection () () IO a)
+  deriving (Functor, Applicative, Monad)
+
+connection :: Atom Connection
+connection = Atom ask
+
+unsafeAtomIO :: IO a -> Atom a
+unsafeAtomIO a = Atom $ liftIO a
+
+runAtom :: Connection -> Atom a -> IO a
+runAtom conn (Atom atom) =
+  fst <$> evalRWST atom conn ()
+
+atomically :: Connection -> Atom a -> IO a
+atomically conn atom = withTransactionSerializable conn $ runAtom conn atom
--- src/Distributed/STM/PVar.hs
+++ src/Distributed/STM/PVar.hs
@@ -1,3 +1,19 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Distributed.STM.PVar where
 
+import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.SqlQQ
 
+import           Distributed.STM
+
+data PVar a = PVar String
+
+newPVar :: String -> a -> Atom (PVar a)
+newPVar label val = do
+  c <- connection
+  unsafeAtomIO $ execute c
+     [sql| INSERT INTO variable (label, value)
+           VALUES (?, ?) |]
+     (label, Binary $ encode val)
+  return $ PVar label
