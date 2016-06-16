072caba1e168f6bc69d762ab0b77b1bf0ffcbb6d
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-06-16 18:27:05 +0300

    WIP

--- src/Distributed/STM/PVar.hs
+++ src/Distributed/STM/PVar.hs
@@ -2,6 +2,10 @@
 
 module Distributed.STM.PVar where
 
+import           Control.Monad                      (void)
+
+import           Data.Aeson
+
 import           Database.PostgreSQL.Simple
 import           Database.PostgreSQL.Simple.SqlQQ
 
@@ -9,11 +13,35 @@ import           Distributed.STM
 
 data PVar a = PVar String
 
-newPVar :: String -> a -> Atom (PVar a)
+newPVar :: ToJSON a => String -> a -> Atom (PVar a)
 newPVar label val = do
   c <- connection
   unsafeAtomIO $ execute c
      [sql| INSERT INTO variable (label, value)
            VALUES (?, ?) |]
-     (label, Binary $ encode val)
+     (label, encode val)
   return $ PVar label
+
+readPVar :: FromJSON a => PVar a -> Atom a
+readPVar (PVar label) = do
+  c <- connection
+  x <- unsafeAtomIO $ query c
+     [sql| SELECT value FROM variable
+           WHERE label = ? |]
+     (Only label)
+  case x of
+    (Only x:_) -> case decode x of
+      Just x -> return x
+      Nothing -> error "PVar can't be parsed"
+    _ -> error "PVar doesn't exist"
+
+
+writePVar :: ToJSON a => PVar a -> a -> Atom ()
+writePVar (PVar label) val = do
+  c <- connection
+  void $ unsafeAtomIO $ do
+    execute c [sql| DELETE FROM variable WHERE label = ? |] (Only label)
+    execute c
+       [sql| INSERT INTO variable (label, value)
+             VALUES (?, ?) |]
+       (label, encode val)
